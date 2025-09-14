2b5f99fabef100685d47b1e2cb539112db73f79e
Design SV <mwas9611@gmail.com>
2025-09-14 11:10:25 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ function pauseRide() {
   clearInterval(timerInterval);
   // Do NOT clear geolocation watch here!
  document.getElementById("ride-btn").innerText = "RESUME";
-  
+
   document.getElementById("end-btn").classList.remove("hidden"); 
   speak("Ride paused");
 }
@@ -92,7 +92,7 @@ function startRide() {
   timerInterval = setInterval(updateTime,1000);
   watchId = navigator.geolocation.watchPosition(handlePos, handleError, {enableHighAccuracy:true,maximumAge:1000,timeout:5000});
   document.getElementById("ride-btn").innerText="STOP";
-  document.getElementById("status").innerText = "Recording";
+
   document.getElementById("end-btn").classList.remove("hidden"); 
   speak("Ride started");
 }
